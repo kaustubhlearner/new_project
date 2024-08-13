795eddb3363a1f690f7c73264edf6472cfb782dd
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-08-13 08:45:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let logo = document.querySelector(".container .logo")
+let active = document.querySelector(".container .gallery")
+
+logo.addEventListener("click",()=>{
+  active.classList.toggle("active")
+ 
+}) 
